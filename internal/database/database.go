808a687a47ca1dbebfbb378cc17a7ff2736b2dc3
thesavides <chris@ukuva.com>
2025-12-08 19:01:15 +0200

    Complete database.go file

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,86 +1,111 @@
 package database
 
 import (
-	"database/sql"
-	"fmt"
-	"time"
+"database/sql"
+"fmt"
+"time"
 
-	"github.com/golang-migrate/migrate/v4"
-	migratepostgres "github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
-	_ "github.com/lib/pq"
-	"github.com/rs/zerolog/log"
-	"github.com/ukuvago/angelvault/internal/config"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+"github.com/golang-migrate/migrate/v4"
+migratepostgres "github.com/golang-migrate/migrate/v4/database/postgres"
+_ "github.com/golang-migrate/migrate/v4/source/file"
+_ "github.com/lib/pq"
+"github.com/rs/zerolog/log"
+"github.com/ukuvago/angelvault/internal/config"
+"gorm.io/driver/postgres"
+"gorm.io/gorm"
+"gorm.io/gorm/logger"
 )
 
 var db *gorm.DB
 
-// Connect establishes a connection to the PostgreSQL database
 func Connect(cfg *config.Config) (*gorm.DB, error) {
-	logLevel := logger.Warn
-	if cfg.IsDevelopment() {
-		logLevel = logger.Info
-	}
-
-	gormConfig := &gorm.Config{
-		Logger: logger.Default.LogMode(logLevel),
-		NowFunc: func() time.Time {
-			return time.Now().UTC()
-		},
-	}
-
-	var err error
-	db, err = gorm.Open(postgres.Open(cfg.DatabaseURL), gormConfig)
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to database: %w", err)
-	}
-
-	// Get underlying SQL DB to configure connection pool
-	sqlDB, err := db.DB()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get underlying SQL DB: %w", err)
-	}
-
-	// Configure connection pool
-	sqlDB.SetMaxIdleConns(10)
-	sqlDB.SetMaxOpenConns(100)
-	sqlDB.SetConnMaxLifetime(time.Hour)
-
-	// Test connection
-	if err := sqlDB.Ping(); err != nil {
-		return nil, fmt.Errorf("failed to ping database: %w", err)
-	}
-
-	log.Info().Msg("Database connection established")
-	return db, nil
+logLevel := logger.Warn
+if cfg.IsDevelopment() {
+logLevel = logger.Info
 }
 
-// GetDB returns the database instance
+gormConfig := &gorm.Config{
+Logger: logger.Default.LogMode(logLevel),
+NowFunc: func() time.Time {
+return time.Now().UTC()
+},
+}
+
+var err error
+db, err = gorm.Open(postgres.Open(cfg.DatabaseURL), gormConfig)
+if err != nil {
+return nil, fmt.Errorf("failed to connect to database: %w", err)
+}
+
+sqlDB, err := db.DB()
+if err != nil {
+return nil, fmt.Errorf("failed to get underlying SQL DB: %w", err)
+}
+
+sqlDB.SetMaxIdleConns(10)
+sqlDB.SetMaxOpenConns(100)
+sqlDB.SetConnMaxLifetime(time.Hour)
+
+if err := sqlDB.Ping(); err != nil {
+return nil, fmt.Errorf("failed to ping database: %w", err)
+}
+
+log.Info().Msg("Database connection established")
+return db, nil
+}
+
 func GetDB() *gorm.DB {
-	return db
+return db
 }
 
-// RunMigrations runs database migrations
 func RunMigrations(cfg *config.Config, migrationsPath string) error {
-	sqlDB, err := sql.Open("postgres", cfg.DatabaseURL)
-	if err != nil {
-		return fmt.Errorf("failed to open database for migrations: %w", err)
-	}
-	defer sqlDB.Close()
+sqlDB, err := sql.Open("postgres", cfg.DatabaseURL)
+if err != nil {
+return fmt.Errorf("failed to open database for migrations: %w", err)
+}
+defer sqlDB.Close()
 
-	driver, err := migratepostgres.WithInstance(sqlDB, &migratepostgres.Config{})
-	if err != nil {
-		return fmt.Errorf("failed to create migration driver: %w", err)
-	}
+driver, err := migratepostgres.WithInstance(sqlDB, &migratepostgres.Config{})
+if err != nil {
+return fmt.Errorf("failed to create migration driver: %w", err)
+}
 
-	m, err := migrate.NewWithDatabaseInstance(
-		fmt.Sprintf("file://%s", migrationsPath),
-		"postgres",
-		driver,
-	)
-	if err != nil {
-		return fmt.Errorf("failed to create migration instance: %w", err)
-	}
+m, err := migrate.NewWithDatabaseInstance(
+fmt.Sprintf("file://%s", migrationsPath),
+"postgres",
+driver,
+)
+if err != nil {
+return fmt.Errorf("failed to create migration instance: %w", err)
+}
+
+if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+return fmt.Errorf("failed to run migrations: %w", err)
+}
+
+version, dirty, _ := m.Version()
+log.Info().Uint("version", version).Bool("dirty", dirty).Msg("Migrations completed")
+return nil
+}
+
+func Close() error {
+if db != nil {
+sqlDB, err := db.DB()
+if err != nil {
+return err
+}
+return sqlDB.Close()
+}
+return nil
+}
+
+func HealthCheck() error {
+if db == nil {
+return fmt.Errorf("database not initialized")
+}
+sqlDB, err := db.DB()
+if err != nil {
+return err
+}
+return sqlDB.Ping()
+}
